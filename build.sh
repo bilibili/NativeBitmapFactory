0a84a73f597ee1ef99598ec63954e5f16f172b83
ctiao <calmer91@gmail.com>
2014-01-17 15:04:35 +0800

    Refactor

---
+++ build.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+DEST_PATH=../../DanmakuFlameMaster/src/main/libs
+ANDROID_SOURCE_PATH=AndroidSource
+export AOSP_PATH=${PWD}/$ANDROID_SOURCE_PATH/platform
+
+mkdir -p $ANDROID_SOURCE_PATH/platform/external/skia
+mkdir -p $ANDROID_SOURCE_PATH/platform/frameworks/base
+mkdir -p $ANDROID_SOURCE_PATH/platform/system/core
+mkdir -p $ANDROID_SOURCE_PATH/platform/frameworks/native
+
+git clone https://android.googlesource.com/platform/external/skia $ANDROID_SOURCE_PATH/platform/external/skia
+git clone https://android.googlesource.com/platform/frameworks/base $ANDROID_SOURCE_PATH/platform/frameworks/base
+git clone https://android.googlesource.com/platform/system/core $ANDROID_SOURCE_PATH/platform/system/core
+git clone https://android.googlesource.com/platform/frameworks/native $ANDROID_SOURCE_PATH/platform/frameworks/native
+cd $ANDROID_SOURCE_PATH/platform/external/skia
+git pull
+cd -
+cd $ANDROID_SOURCE_PATH/platform/frameworks/base
+git pull
+cd -
+cd $ANDROID_SOURCE_PATH/platform/system/core
+git pull
+cd -
+cd $ANDROID_SOURCE_PATH/platform/frameworks/native
+git pull
+cd -
+
+
+
+checkout_tags() {
+	echo "\n\nexternal/skia: checkout -b $1..."
+	cd $ANDROID_SOURCE_PATH/platform/external/skia
+	git add .
+	git commit -m 'commit'
+	git checkout -b $1 $1
+	git checkout $1 -f
+	git branch
+	cd -
+	
+	echo "\n\nframeworks/base: checkout -b $1..."
+	cd $ANDROID_SOURCE_PATH/platform/frameworks/base
+	git add .
+	git commit -m 'commit'
+	git checkout -b $1 $1
+	git checkout $1 -f
+	git branch
+	cd -
+	
+	echo "\n\nsystem/core: checkout -b $1"
+	cd $ANDROID_SOURCE_PATH/platform/system/core
+	git add .
+	git commit -m 'commit'
+	git checkout -b $1 $1
+	git checkout $1 -f
+	git branch
+	cd -
+	
+	echo "\n\nframeworks/native: checkout -b $1"
+	cd $ANDROID_SOURCE_PATH/platform/frameworks/native
+	git add .
+	git commit -m 'commit'
+	git checkout -b $1 $1
+	git checkout $1 -f
+	git branch
+	cd -
+	
+	echo "====checkout tag ready======"
+}
+
+build_so() {
+	echo "\n\n-----ndk build libskia.so-------- "
+	export API=$1
+	ndk-build
+}
+
+cp_so() {
+	mkdir tmp
+	cp libs tmp -rf
+}
+
+cp_release_so() {
+	rm -rf $DEST_PATH/*/libndkbitmap*.so
+	mkdir -p $DEST_PATH/armeabi
+	yes | cp tmp/libs/armeabi/libndkbitmap*.so $DEST_PATH/armeabi
+	mkdir -p $DEST_PATH/armeabi-v7a
+	yes | cp -rf tmp/libs/armeabi/libndkbitmap*.so $DEST_PATH/armeabi-v7a
+	mkdir -p $DEST_PATH/x86
+	yes | cp -rf tmp/libs/x86/libndkbitmap*.so $DEST_PATH/x86
+	mkdir -p $DEST_PATH/mips
+	yes | cp -rf tmp/libs/mips/libndkbitmap*.so $DEST_PATH/mips
+	
+	rm -rf tmp
+}
+
+rm -rf tmp
+
+checkout_tags android-sdk-support_r11
+build_so 11
+cp_so
+
+checkout_tags android-cts-4.0_r1
+build_so 14
+cp_so
+
+checkout_tags android-4.0.3_r1
+build_so 15
+cp_so
+
+checkout_tags android-4.1.2_r1
+build_so 16
+cp_so
+
+checkout_tags android-4.2.2_r1
+build_so 17
+cp_so
+
+checkout_tags android-4.3_r1
+build_so 18
+cp_so
+
+checkout_tags android-4.4.2_r1
+cp $ANDROID_SOURCE_PATH/platform/external/skia/include/lazy/SkBitmapFactory.h $ANDROID_SOURCE_PATH/platform/external/skia/include/core/SkBitmapFactory.h #file missing
+build_so 19
+cp_so
+
+cp_release_so
+
+echo "BUILD SUCCESS"
---
+++ jni/NativeBitmapFactory.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <jni.h>
+#include <SkBitmap.h>
+#include <GraphicsJNI.h>
+//#include <android/log.h>
+#include "NativeBitmapFactory.h"
+
+jobject createBitmap(JNIEnv * env ,jobject  obj,jint w,jint h,SkBitmap::Config config,jboolean hasAlpha,int isMuttable)
+{
+SkBitmap* bm = new SkBitmap();
+bm->setConfig(config, w, h);
+bm->setIsOpaque(!hasAlpha);
+bm->allocPixels();
+bm->eraseColor(0);
+//__android_log_print(ANDROID_LOG_DEBUG, "NativeBitmap", "Created bitmap %d has width = %d, height = %d",bm, bm->width(), bm->height());
+jobject result = GraphicsJNI::createBitmap(env,bm,isMuttable,NULL,-1);
+return result;
+}
+
+jobject Java_tv_cjump_jni_NativeBitmapFactory_createBitmap(JNIEnv * env ,jobject  obj,jint w,jint h,SkBitmap::Config config,jboolean hasAlpha)
+{
+return createBitmap(env,obj,w,h,config,hasAlpha,true);
+}
+
+jobject Java_tv_cjump_jni_NativeBitmapFactory_createBitmap19(JNIEnv * env ,jobject  obj,jint w,jint h,SkBitmap::Config config,jboolean hasAlpha)
+{
+return createBitmap(env,obj,w,h,config,hasAlpha,0x3);
+}
