02e704b65f30ddcb13b7df4b042a96eed45b4fcd
ctiao <calmer91@gmail.com>
2014-07-07 10:29:12 +0800

    build.sh: add github-remote-url

--- build.sh
+++ build.sh
@@ -1,46 +1,65 @@
-#git clone https://android.googlesource.com/platform/external/skia AndroidSource/platform/external/skia
-#git clone https://android.googlesource.com/platform/frameworks/base AndroidSource/platform/frameworks/base
-#git clone https://android.googlesource.com/platform/system/core AndroidSource/platform/system/core
-#git clone https://android.googlesource.com/platform/frameworks/native AndroidSource/platform/frameworks/native
-cd AndroidSource/platform/external/skia
+#!/bin/sh
+
+DEST_PATH=../../DanmakuFlameMaster/src/main/libs
+ANDROID_SOURCE_PATH=AndroidSource
+export AOSP_PATH=${PWD}/$ANDROID_SOURCE_PATH/platform
+
+mkdir -p $ANDROID_SOURCE_PATH/platform/external/skia
+mkdir -p $ANDROID_SOURCE_PATH/platform/frameworks/base
+mkdir -p $ANDROID_SOURCE_PATH/platform/system/core
+mkdir -p $ANDROID_SOURCE_PATH/platform/frameworks/native
+
+git clone https://github.com/ctiao/platform-external-skia.git $ANDROID_SOURCE_PATH/platform/external/skia
+git clone https://github.com/ctiao/platform-frameworks-base.git $ANDROID_SOURCE_PATH/platform/frameworks/base
+git clone https://github.com/ctiao/platform-system-core.git $ANDROID_SOURCE_PATH/platform/system/core
+git clone https://github.com/ctiao/platform-frameworks-native.git $ANDROID_SOURCE_PATH/platform/frameworks/native
+cd $ANDROID_SOURCE_PATH/platform/external/skia
 git pull
 cd -
-cd AndroidSource/platform/frameworks/base
+cd $ANDROID_SOURCE_PATH/platform/frameworks/base
 git pull
 cd -
-cd AndroidSource/platform/system/core
+cd $ANDROID_SOURCE_PATH/platform/system/core
 git pull
 cd -
-cd AndroidSource/platform/frameworks/native
+cd $ANDROID_SOURCE_PATH/platform/frameworks/native
 git pull
 cd -
 
-export ANDROID_SOURCE_PATH=AndroidSource
 
-function checkout_tags(){
-	echo "external/skia: checkout -b $1..."
+
+checkout_tags() {
+	echo "\n\nexternal/skia: checkout -b $1..."
 	cd $ANDROID_SOURCE_PATH/platform/external/skia
+	git add .
+	git commit -m 'commit'
 	git checkout -b $1 $1
 	git checkout $1 -f
 	git branch
 	cd -
 	
-	echo "frameworks/base: checkout -b $1..."
+	echo "\n\nframeworks/base: checkout -b $1..."
 	cd $ANDROID_SOURCE_PATH/platform/frameworks/base
+	git add .
+	git commit -m 'commit'
 	git checkout -b $1 $1
 	git checkout $1 -f
 	git branch
 	cd -
 	
-	echo "system/core: checkout -b $1"
+	echo "\n\nsystem/core: checkout -b $1"
 	cd $ANDROID_SOURCE_PATH/platform/system/core
+	git add .
+	git commit -m 'commit'
 	git checkout -b $1 $1
 	git checkout $1 -f
 	git branch
 	cd -
 	
-	echo "frameworks/native: checkout -b $1"
+	echo "\n\nframeworks/native: checkout -b $1"
 	cd $ANDROID_SOURCE_PATH/platform/frameworks/native
+	git add .
+	git commit -m 'commit'
 	git checkout -b $1 $1
 	git checkout $1 -f
 	git branch
@@ -49,27 +68,27 @@ function checkout_tags(){
 	echo "====checkout tag ready======"
 }
 
-function build_so(){
-	echo "-----ndk build libskia.so-------- "
+build_so() {
+	echo "\n\n-----ndk build libskia.so-------- "
 	export API=$1
 	ndk-build
 }
 
-function cp_so(){
+cp_so() {
 	mkdir tmp
 	cp libs tmp -rf
 }
 
-function cp_release_so(){
-	rm -rf libs
-	mkdir -p libs/armeabi
-	yes | cp tmp/libs/armeabi/libndkbitmap*.so libs/armeabi
-	mkdir -p libs/armeabi-v7a
-	yes | cp -rf tmp/libs/armeabi/libndkbitmap*.so libs/armeabi-v7a
-	mkdir -p libs/x86
-	yes | cp -rf tmp/libs/x86/libndkbitmap*.so libs/x86
-	mkdir -p libs/mips
-	yes | cp -rf tmp/libs/mips/libndkbitmap*.so libs/mips
+cp_release_so() {
+	rm -rf $DEST_PATH/*/libndkbitmap*.so
+	mkdir -p $DEST_PATH/armeabi
+	yes | cp tmp/libs/armeabi/libndkbitmap*.so $DEST_PATH/armeabi
+	mkdir -p $DEST_PATH/armeabi-v7a
+	yes | cp -rf tmp/libs/armeabi/libndkbitmap*.so $DEST_PATH/armeabi-v7a
+	mkdir -p $DEST_PATH/x86
+	yes | cp -rf tmp/libs/x86/libndkbitmap*.so $DEST_PATH/x86
+	mkdir -p $DEST_PATH/mips
+	yes | cp -rf tmp/libs/mips/libndkbitmap*.so $DEST_PATH/mips
 	
 	rm -rf tmp
 }
@@ -106,3 +125,5 @@ build_so 19
 cp_so
 
 cp_release_so
+
+echo "BUILD SUCCESS"
